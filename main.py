5a568ca8c119a5cc9b5f396af6ec06da54d0ff2f
Masih <masih.zn.mz@gmail.com>
2025-08-11 17:14:36 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# import dejavu
+
+# from dejavu import Dejavu
+config = {
+     "database": {
+        "host": "127.0.0.1",
+        "user": "root",
+        "password": "dejavu",
+        "database": "password",
+    }
+}
+# djv = Dejavu(config)
+
+
